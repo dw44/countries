67678e2bfe8e13ea94a1d6ea27e2d94a46ed08f1
Tanzeel <35539906+dw44@users.noreply.github.com>
2021-05-21 16:49:18 -0400

    created context for theme toggling

---
+++ context/ThemeContext.js
@@ -0,0 +1,37 @@
+import { createContext, useReducer } from 'react';
+
+const startingTheme = {
+  dark: false
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'TOGGLE_THEME':
+      return {
+        dark: !state.dark
+      }
+    default:
+      return state;
+  }
+}
+
+export const ThemeContext = createContext(startingTheme);
+
+export const ThemeProvider = props => {
+  const [state, dispatch] = useReducer(reducer, startingTheme);
+  
+  const toggleTheme = () => {
+    dispatch({
+      type: 'TOGGLE_THEME'
+    });
+  }
+
+  return (
+    <ThemeContext.Provider value={{
+      dark: state.dark,
+      toggleTheme
+    }}>
+      { props.children }
+    </ThemeContext.Provider>
+  );
+}
